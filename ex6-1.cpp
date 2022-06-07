0b601bcaec5eff91b4650d18402edad974290019
hiouo <68846490+hiouo@users.noreply.github.com>
2022-06-07 10:55:13 +0800

    Create ex6-1.cpp

---
+++ ex6-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cin >> x;
+    cin >> y;
+    if(x > y){
+        std::cout << x << std::endl;
+    } else{
+        std::cout << y << std::endl;
+    }
+}
